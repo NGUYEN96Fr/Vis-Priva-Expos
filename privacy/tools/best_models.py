ab82937d2b4fed60b67d064ef7e4b58ed2fb2940
NGUYEN96Fr <vankhoa96pfiev@gmail.com>
2021-03-14 20:58:53 +0700

    fix bug usrs without images; add random vis concept selection study

--- privacy/tools/best_models.py
+++ privacy/tools/best_models.py
@@ -15,7 +15,10 @@ The module verifies best trained models for situations within detectors
 import os
 import pickle
 import argparse
+from pathlib import Path
 import _init_paths
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def argument_parser():
@@ -26,9 +29,10 @@ def argument_parser():
         argparse.ArgumentParser
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", required= True, help= "path to trained models")
+    parser.add_argument("--path", required=True, help="path to trained models")
 
     return parser
+
 
 def main():
     """
@@ -41,38 +45,47 @@ def main():
     sdetecs = os.listdir(args.path)
     mobis = []
     rcnns = []
+    # directory to save the best configuration
+    cfg_dir = os.path.join(Path(args.path).parent, "Bcfg_"+args.path.split("/")[-1].split("models_")[-1])
+
+    if not os.path.isdir(cfg_dir):
+        os.mkdir(cfg_dir)
 
     for sdetec in sdetecs:
         # list of trained models
         spath = os.path.join(args.path, sdetec)
         models = os.listdir(spath)
         best_result = -1
+        best_cfg = None
 
         for model in models:
             mpath = os.path.join(spath, model)
             # loaded model
             lmodel = pickle.load(open(mpath, 'rb'))
             lmodel.set_seeds()
-            lmodel.test_vispel()
+            lmodel.test_vispel(half_vis=True)
             test_result = lmodel.test_result
 
             if test_result > best_result:
                 best_result = test_result
-                best_path = mpath
+                best_cfg = lmodel.cfg
+
         print('#-----------------------#')
         print(sdetec)
+        print(best_result)
+
+        with open(os.path.join(cfg_dir, sdetec+".pkl"), 'wb') as handle:
+            pickle.dump(best_cfg, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
         if 'rcnn' in sdetec:
             rcnns.append(best_result)
 
         if 'mobi' in sdetec:
             mobis.append(best_result)
 
-        print(best_result)
-        # best_model = pickle.load(open(best_path, 'rb'))
-        # print(best_model.cfg)
-
     print('RCNN avg: ',sum(rcnns)/len(rcnns))
     print('MOBI avg: ',sum(mobis)/len(mobis))
+
 
 if __name__ == '__main__':
     main()
